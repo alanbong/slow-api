386147d24f7c9f4e07044c1018f4e02a63dfe51c
alanbong <bochkarevdenis1995@gmail.com>
2026-04-20 21:17:32 +0300

    feat(views): add new Views

--- views.py
+++ views.py
@@ -1,63 +1,62 @@
 # views.py
-from core.templator import render
+from models import User, Car
+from core.views import BaseView
 
-def get_data_from_file(filename):
-    data = []
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            if line.strip():
-                data.append(line.strip().split('::'))
-    return data
 
 def home_view():
-    body = 'home'
+    body = 'home'.encode('utf-8')
     return '200 OK', [('Content-Type', 'text/html')], body
 
-def users_list_view():
-    users_data = get_data_from_file('data/users.txt')
-    users = [{'id': user[0], 'name': user[1]} for user in users_data]
-    context = {'users': users}
 
-    template = """
-    <h1>Our users:</h1>
-    <ul>
-        {% for user in users %}
-            <li>ID: {{ user.id }}, Name: {{ user.name }}</li>
-        {% endfor %}
-    </ul>
-    """
-    body = render(template, context).encode('utf-8')
-    return '200 OK', [('Content-Type', 'text/html')], body
+class UserListView(BaseView):
+    template_name = 'users.txt'
 
-def user_detail_view(user_id):
-    all_cars = get_data_from_file('data/cars.txt')
-    user_cars = [
-        {'id': car[0], 'brand': car[2], 'model': car[3], 'plate': car[4]} 
-        for car in all_cars if int(car[1]) == user_id
-    ]
-    context = {'cars': user_cars}
-    template = """
-    <h1>Your cars:</h1>
-    <ul>
-        {% for car in cars %}
-            <li>{{ car.brand }} {{ car.model }} ({{ car.plate }})</li>
-        {% endfor %}
-    </ul>
-    """
+    def get(self):
+        users = User().all()
+        return self.render({'users': users})
 
-    body = render(template, context).encode('utf-8')
-    return '200 OK', [('Content-Type', 'text/html')], body
+
+class UserCreateView(BaseView):
+    template_name = 'user_form.txt'
+
+    def get(self):
+        return self.render({})
+
+    def post(self, request_data):
+        User().create(**request_data)
+        return self.redirect('/users/')
+
+
+class UserDetailView(BaseView):
+    template_name = 'user_detail.txt'
+
+    def get(self, user_id):
+        user = User().get(user_id)
+        user_cars = Car().filter(uid=user_id)
+        return self.render({'user': user, 'cars': user_cars})
+
+    def patch(self, request_data, user_id):
+        User().update(user_id, request_data)
+        return self.redirect(f'/users/{user_id}/')
+
+    def delete(self, user_id):
+        User().delete(user_id)
+        return self.redirect('/users/')
+
 
 def not_found_view():
+    body = 'Not Found'.encode('utf-8')
     return (
         '404 Not Found',
         [('content-type', 'text/html')],
-        'Not Found'
+        body
     )
 
+
 def method_not_allowed(allowed_methods=None):
+    body = f'Allowed methods: {allowed_methods}'
     return (
         '405 Method Not Allowed',
         [('content-type', 'text/html')],
-        f'Allowed methods: {allowed_methods}'
+        body
     )
