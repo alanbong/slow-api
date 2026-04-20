e47cab87c73c5de474432a19d3aeec4ead37a4a2
alanbong <bochkarevdenis1995@gmail.com>
2026-04-20 21:18:45 +0300

    feat(wsgi): add support for requests with body with Request class

--- wsgi.py
+++ wsgi.py
@@ -1,26 +1,20 @@
-#wsgi.py
+# wsgi.py
 from wsgiref.simple_server import make_server
 
+from core.request import Request
 from urls import router
 
 
-def app(req, res):
-    method, path = get_params(req)
-    handler, params = router.get_handler(path, method)
-    status, headers, body = handler(**params)
-
-    if isinstance(body, str):
-        body = body.encode('utf-8')
-
-    headers.append(('Content-Length', str(len(body))))
+def app(env, res):
+    request = Request(env)
+    view, params = router.get_handler(request.path, request.method)
+    if request.method in ('POST', 'PATCH', 'PUT'):
+        status, headers, body = view(request.data, **params)
+    else:
+        status, headers, body = view(**params)
 
     res(status, headers)
     return [body]
-
-def get_params(req):
-    method = req.get('REQUEST_METHOD', None)
-    path = req.get('PATH_INFO', None)
-    return method, path
 
 httpd = make_server('localhost', 8080, app)
 httpd.serve_forever()
